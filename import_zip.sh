65fca8ecf2ac2c468bc2be1b84b338165f617edc
Jim McAndrew <jim@loc8.us>
2015-01-04 15:27:12 -0700

    Clean up and add comments

--- import_zip.sh
+++ import_zip.sh
@@ -1,12 +1,21 @@
 #!/bin/bash
 
+if [ -n "$1"]; then
+  echo "USAGE: import_zip.sh ZIP_FILE"
+  exit 1;
+fi
+
+# Determine the directory where this script resides
 DIR=$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )
 zip_path="$1"
 import_raster="$DIR"/import_raster.sh
 zip_name="${zip_path##*/}"
 temp_path="${zip_path%/*}"/.tmp_"${zip_name%.*}"
 
+# Unzip the file, and find the first .pdf file inside the zip directory
+# TODO: this can be changed to deal with more files, but the USGS zip files only contain a single pdf
 unzip "$zip_path" -d "$temp_path" && \
-  /bin/bash "$import_raster" "$temp_path"/"`ls \"$temp_path\" | /bin/grep .pdf`"
+  /bin/bash "$import_raster" "$temp_path"/"`ls \"$temp_path\" | /bin/grep .pdf | head -n 1`"
 
-rm -rf $temp_path
+# Remove the unzipped file after we're done processing it
+rm -rf "$temp_path"
